9567b4c8f1d7712768e1807d41e4f7f627357ab0
diardomarendikrista <diardomarendikrista@gmail.com>
2022-02-02 21:58:23 +0700

    set default API

--- src/socket/ws.js
+++ src/socket/ws.js
@@ -1,5 +1,5 @@
 // ws.js
 import io from 'socket.io-client';
 
-// export const ws = io('https://petekchat.herokuapp.com');
-export const ws = io('http://localhost:4000');
+export const ws = io('https://petekchat.herokuapp.com');
+// export const ws = io('http://localhost:4000');
